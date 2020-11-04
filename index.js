9c83dbdd9f93c2a341031c19e2f822bf6e8399be
Carlos Molmelstet <molmelstet2016@gmail,com>
2020-11-04 20:29:37 -0300

    commit teste foda

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-
-app.get("/", (req, res) => {
-    res.send("<h1>DEU BOMM KRLL</h1>")
-})
-
-app.listen(3000, () => console.log('Server deu bom'))
