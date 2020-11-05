28318c6052bce99e93c83637364b01ae51f53e9d
Carlos Molmelstet <64798175+carlosmolmelstet@users.noreply.github.com>
2020-11-04 21:11:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,36 +3,15 @@ var app = express();
 
 
 app.get("/", (req, res) => {
-    res.json([
+    res.json(
         {
             id: 1,
             name: "camisa 1",
             description: "camiseta bonita e legal",
             price: 55.99,
             active: true
-        },
-        {
-            id: 2,
-            name: "camisa 2",
-            description: "camiseta muitooo bonita e legal",
-            price: 40.99,
-            active: true
-        },
-        {
-            id: 3,
-            name: "camisa 3",
-            description: "camiseta feia e triste",
-            price: 100.99,
-            active: false
-        },
-        {
-            id: 4,
-            name: "camisa 4",
-            description: "camiseta foda pra krll",
-            price: 55.99,
-            active: true
-        },
-    ])
+        }
+    )
 })
 
 app.listen(3000, () => console.log('Server deu bom'))
